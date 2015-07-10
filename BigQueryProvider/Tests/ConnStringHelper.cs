27d813334ee0e203ba11642d7bf37b7a52408709
Agitto <agitto@yandex.ru>
2015-07-10 13:21:34 +0300

    gitignore *.p12 file

--- BigQueryProvider/Tests/ConnStringHelper.cs
+++ BigQueryProvider/Tests/ConnStringHelper.cs
@@ -1,19 +1,10 @@
-﻿using System.Data.Common;
+﻿using System.Configuration;
+using System.Data.Common;
 
 namespace DevExpress.DataAccess.BigQuery.Tests {
     public static class ConnStringHelper {
-        static readonly DbConnectionStringBuilder connectionStringBuilder = new DbConnectionStringBuilder();
-
         public static string ConnectionString {
-            get { return connectionStringBuilder.ConnectionString.ToLower(); }
-        }
-
-
-        static ConnStringHelper() {
-            connectionStringBuilder["PrivateKeyFileName"] = @"..\..\zymosimeter-e34a09c6f230.p12";
-            connectionStringBuilder["ProjectID"] = "zymosimeter";
-            connectionStringBuilder["ServiceAccountEmail"] = "227277881286-l0fodnq2h35m58b80up9vi4g83p1ogus@developer.gserviceaccount.com";
-            connectionStringBuilder["DataSetId"] = "testdata";
+            get { return ConfigurationManager.ConnectionStrings["bigqueryConnectionString"].ConnectionString; }
         }
     }
 }

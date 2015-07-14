f1a09716d303085d973fff00e4b160562160d312
Agitto <agitto@yandex.ru>
2015-07-14 16:43:20 +0300

    fix failed merge

--- BigQueryProvider/BigQueryConnection.cs
+++ BigQueryProvider/BigQueryConnection.cs
@@ -129,12 +129,6 @@ namespace DevExpress.DataAccess.BigQuery {
                 HttpClientInitializer = credential,
                 ApplicationName = "DevExpress.DataAccess.BigQuery ADO.NET Provider"
             });
-<<<<<<< HEAD
-=======
-            JobsResource.ListRequest listRequest = Service.Jobs.List(ProjectId);
-            await listRequest.ExecuteAsync().ConfigureAwait(false);
-            state = ConnectionState.Open;
->>>>>>> 5efe8c29274c95e55e8f06bbe8d3fa9eabf3eebf
         }
 
         public override void Close() {

a1da11c98a60b1c635ad828c6505f95e267e1040
sady4850@gmail.com <sady4850@gmail.com>
2015-07-21 17:56:37 +0300

    tests clean-up

---
+++ BigQueryProvider/Tests/BigQueryTestsOAuth.cs
@@ -0,0 +1,7 @@
+﻿namespace DevExpress.DataAccess.BigQuery.Tests {
+    public class BigQueryTestsOAuth : BigQueryTestsBase {
+        protected override string GetConnectionString() {
+            return ConnectionStringHelper.OAuthConnectionString;
+        }
+    }
+}

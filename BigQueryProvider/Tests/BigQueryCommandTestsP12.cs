da057d60b185b70a622092b973d216710ea9287a
sady4850@gmail.com <sady4850@gmail.com>
2015-07-21 17:19:12 +0300

    tests clean-up

---
+++ BigQueryProvider/Tests/BigQueryCommandTestsP12.cs
@@ -0,0 +1,7 @@
+﻿namespace DevExpress.DataAccess.BigQuery.Tests {
+    public class BigQueryCommandTestsP12 : BigQueryCommandTestsBase {
+        protected override string GetConnectionString() {
+            return ConnectionStringHelper.P14ConnectionString;
+        }
+    }
+}

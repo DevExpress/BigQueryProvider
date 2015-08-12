2f02f05924b2ec2bd6fe5f814c26bbc3001820c2
Anton Repushko <repushko.a@gmail.com>
2015-08-12 18:45:44 +0300

    cleanup csproj

--- BigQueryProvider/Tests/BigQueryConnectionTests.cs
+++ BigQueryProvider/Tests/BigQueryConnectionTests.cs
@@ -12,7 +12,6 @@ namespace DevExpress.DataAccess.BigQuery.Tests {
                 Assert.True(string.Equals(ConnectionStringHelper.OAuthConnectionString, connection.ConnectionString, StringComparison.OrdinalIgnoreCase));
                 Assert.Equal(ConnectionState.Closed, connection.State);
                 Assert.Null(connection.Service);
-                
                 connection.Open();
                 Assert.NotNull(connection.ConnectionString);
                 Assert.True(string.Equals(ConnectionStringHelper.OAuthConnectionString, connection.ConnectionString, StringComparison.OrdinalIgnoreCase));

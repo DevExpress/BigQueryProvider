a9f43b8824758617e5cff4ecccb9424c089971cf
Vyacheslav Zaikin <agitto@yandex.ru>
2015-08-25 16:21:32 +0300

    add file

---
+++ BigQueryProvider/Native/ObjectUtils.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+namespace DevExpress.DataAccess.BigQuery.Native {
+    public static class ObjectUtils {
+        public static string ToInvariantString(this object value) {
+            return ToInvariantString(value, "{0}");
+        }
+
+        public static string ToInvariantString(this object value, string format) {
+            return string.Format(CultureInfo.InvariantCulture, format, value);
+        }
+    }
+}

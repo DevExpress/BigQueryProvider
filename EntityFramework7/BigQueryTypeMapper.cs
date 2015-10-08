0afa0235dcad776a79a5ab7d3bfc9b04c31af359
Aristarh71 <dmitry.feofilov@outlook.com>
2015-10-08 12:17:34 +0300

    Clean-up

--- EntityFramework7/BigQueryTypeMapper.cs
+++ EntityFramework7/BigQueryTypeMapper.cs
@@ -19,8 +19,11 @@ namespace DevExpress.DataAccess.BigQuery.EntityFarmework7 {
         readonly RelationalTypeMapping mappingTimestamp = new RelationalTypeMapping("timestamp", DbType.DateTime);
         readonly RelationalTypeMapping mappingRecord = new RelationalTypeMapping("record", DbType.Object);
 
-        readonly Dictionary<string, RelationalTypeMapping> simpleNameMappings;
         readonly Dictionary<Type, RelationalTypeMapping> simpleMappings;
+        readonly Dictionary<string, RelationalTypeMapping> simpleNameMappings;
+
+        protected override IReadOnlyDictionary<Type, RelationalTypeMapping> SimpleMappings { get { return this.simpleMappings; } }
+        protected override IReadOnlyDictionary<string, RelationalTypeMapping> SimpleNameMappings { get { return this.simpleNameMappings; } }
 
         public BigQueryTypeMapper() {
             this.simpleNameMappings = new Dictionary<string, RelationalTypeMapping>(StringComparer.OrdinalIgnoreCase) {
@@ -63,34 +66,5 @@ namespace DevExpress.DataAccess.BigQuery.EntityFarmework7 {
         protected override string GetColumnType(IProperty property) {
             return property.BigQuery().ColumnType;
         }
-
-        public override RelationalTypeMapping GetDefaultMapping(Type clrType) {
-            Check.NotNull(clrType, nameof(clrType));
-            return base.GetDefaultMapping(clrType);
-        }
-
-        protected override RelationalTypeMapping GetCustomMapping(IProperty property) {
-            RelationalTypeMapping relationalTypeMapping = base.GetCustomMapping(property);
-            return relationalTypeMapping;
-        }
-
-        public override RelationalTypeMapping MapPropertyType(IProperty property) {
-            RelationalTypeMapping relationalTypeMapping = base.MapPropertyType(property);
-            return relationalTypeMapping;
-        }
-
-        protected override RelationalTypeMapping MapString(IProperty property, int maxBoundedLength, Func<int, RelationalTypeMapping> boundedMapping, RelationalTypeMapping unboundedMapping, RelationalTypeMapping defaultMapping, RelationalTypeMapping keyMapping = null) {
-            RelationalTypeMapping relationalTypeMapping = base.MapString(property, maxBoundedLength, boundedMapping, unboundedMapping, defaultMapping, keyMapping);
-            return relationalTypeMapping;
-        }
-
-        protected override RelationalTypeMapping MapByteArray(IProperty property, int maxBoundedLength, Func<int, RelationalTypeMapping> boundedMapping, RelationalTypeMapping unboundedMapping, RelationalTypeMapping defaultMapping, RelationalTypeMapping keyMapping = null, RelationalTypeMapping rowVersionMapping = null) {
-            RelationalTypeMapping relationalTypeMapping = base.MapByteArray(property, maxBoundedLength, boundedMapping, unboundedMapping, defaultMapping, keyMapping, rowVersionMapping);
-            return relationalTypeMapping;
-        }
-
-        protected override IReadOnlyDictionary<Type, RelationalTypeMapping> SimpleMappings { get { return this.simpleMappings; } }
-
-        protected override IReadOnlyDictionary<string, RelationalTypeMapping> SimpleNameMappings { get { return this.simpleNameMappings; } }
     }
 }
